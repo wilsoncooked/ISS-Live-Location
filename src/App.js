0ff25eb4eae6171ccca2ca8a2d248c3dfe97d507
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-24 11:24:42 +0200

    playing around with box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
+import SpecsPanel from './Components/SpecsPanel.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SpecsPanel />
       </div>
     );
   }
 }
 
-export default App
+export default App;
