e2ad39debf5f3563e72db454df29b156fdcc7719
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-29 14:18:04 +0200

    first commit, basic colour scheme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
 import NightMap from './Components/NightMap.js'
+import Themes from './Themes'
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <NightMap />
-      </div>
+      <MuiThemeProvider theme={Themes}>
+        <div className="App">
+          <NightMap />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
