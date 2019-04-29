66792ae3379659e6a3ffa79351143a5d7a492a63
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-29 11:09:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
+import NavigationBar from './Components/NavigationBar.js'
 import NightMap from './Components/NightMap.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavigationBar />
       <NightMap />
       </div>
     );
