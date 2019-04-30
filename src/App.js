a631f7042cfee94c4165a286a1df967756374ef0
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-30 13:28:43 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import IssApi from './Components/IssApi.js'
 import NightMap from './Components/NightMap.js'
 import Themes from './Themes'
@@ -8,12 +9,22 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <MuiThemeProvider theme={Themes}>
         <div className="App">
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={LandingPage}/>
+            <Route path="/map" component={MainMap}/>
+            <Route path="/prediction" component={Prediction}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
           <IssApi />  
           <NightMap />
         </div>
       </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
