35b27bb5be208def1493a5b4741311c30c9f9bca
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-30 11:52:36 +0200

    working and live updating location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import IssApi from './Components/IssApi.js'
 import NightMap from './Components/NightMap.js'
 import Themes from './Themes'
 import { MuiThemeProvider } from '@material-ui/core/styles';
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <MuiThemeProvider theme={Themes}>
         <div className="App">
+          <IssApi />  
           <NightMap />
         </div>
       </MuiThemeProvider>
