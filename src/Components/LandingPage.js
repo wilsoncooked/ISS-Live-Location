388771852f2b7975446e06354b1a689fb32f4076
Neli <diakonidzeneli@gmail.com>
2019-05-01 01:09:07 +0200

    animation satelite added

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -6,6 +6,10 @@ import './LandingPage.css'
 const LandingPage = () => {
   return (
     <div className="container">
+        <div class="planet"></div>
+    <div class="gravity">
+      <div class="satellite"></div>
+    </div>
       <button className="actionbutton">EXPLORE</button>
     </div>
   );
