8c2e4fa7330ac65439f469dfccbc9d0e97b180a6
Neli <diakonidzeneli@gmail.com>
2019-04-30 13:13:38 +0200

    "first version with material ui"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import NightMap from './Components/NightMap.js'
+import LandingPage from './Components/LandingPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <NightMap />
+      <LandingPage />
       </div>
     );
   }
