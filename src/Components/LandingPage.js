46e247b38bd758ec98296e492b5c79f58c71aa11
Neli <diakonidzeneli@gmail.com>
2019-04-30 23:50:44 +0200

    added background and action button

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,131 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import Typography from '@material-ui/core/Typography';
-import { NONAME } from 'dns';
-import { borders } from '@material-ui/system';
-import { width } from '@material-ui/system/sizing';
+import React from 'react'
+import photo1 from '../Photos/photo5jpg.jpg'
+import './LandingPage.css'
 
 
 
-const LandingPage = theme => ({
-  
-  image: {
-    position: 'relative',
-    height: 200,
-    [theme.breakpoints.down('xs')]: {
-      width: '100% !important', // Overrides inline-style
-      height: 100,
-    },
-    '&:hover, &$focusVisible': {
-      zIndex: 1,
-      '& $imageBackdrop': {
-        opacity: 0.15,
-      },
-      '& $imageMarked': {
-        opacity: 0,
-      },
-      '& $imageTitle': {
-        border: '10px solid white',
-        width: 500,
-        height: 50,
-      },
-    },
-  },
-  focusVisible: {},
-  imageButton: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.common.white,
-  },
-  imageSrc: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 40%',
-  },
-  imageBackdrop: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    opacity: 0.4,
-    transition: theme.transitions.create('opacity'),
-  },
-  imageTitle: {
-    position: 'relative',
-    border: roude
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
-  },
-  imageMarked: {
-    height: 3,
-    width: 18,
-    position: 'absolute',
-    bottom: -2,
-    left: 'calc(50% - 9px)',
-    transition: theme.transitions.create('opacity'),
-  },
-});
-
-const images = [
-  {
-    url: 'https://images.unsplash.com/photo-1446776858070-70c3d5ed6758?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80',
-    title: 'ISS-LOCATION',
-    width: '100%',
-    height: '100%',
-  },
-];
-
-function ButtonBases(props) {
-  const { classes } = props;
-
+const LandingPage = () => {
   return (
-    <div className={classes.root}>
-      {images.map(image => (
-        <ButtonBase className= "buttonlp"
-          focusRipple
-          key={image.title}
-          className={classes.image}
-          focusVisibleClassName={classes.focusVisible}
-          style={{
-            width: 800,
-            
-          }}
-        >
-          <span
-            className={classes.imageSrc}
-          />
-          <span className={classes.imageBackdrop} />
-          <span className={classes.imageButton}>
-            <Typography
-              component="span"
-              variant="subtitle1"
-              color="inherit"
-              className={classes.imageTitle}
-            >
-              {image.title}
-              <span className={classes.imageMarked} />
-            </Typography>
-          </span>
-        </ButtonBase>
-      ))}
+    <div className="container">
+      <button className="actionbutton">EXPLORE</button>
     </div>
   );
 }
 
-ButtonBases.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(LandingPage)(ButtonBases);
+export default LandingPage;
