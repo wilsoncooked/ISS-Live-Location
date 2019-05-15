2207df538316178d88209eabc95a5ecb549239f3
Neli <diakonidzeneli@gmail.com>
2019-05-15 14:05:16 +0800

    Updates

---
+++ precache-manifest.c1acbf5d37cb6951c3055f2dbea378c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b8c75df959a1a741e868b2f2b2d2d3",
+    "url": "/ISS-Live-Location/./static/media/satellite.12b8c75d.svg"
+  },
+  {
+    "revision": "6738d77087fba8e2ebcaf4bc0ca1e857",
+    "url": "/ISS-Live-Location/./static/media/photo1.6738d770.jpg"
+  },
+  {
+    "revision": "f2e224272b47fa294538dc14ffaf35cb",
+    "url": "/ISS-Live-Location/./static/media/map2.f2e22427.png"
+  },
+  {
+    "revision": "ef92b64d1d25f049ff4bdc98d171744a",
+    "url": "/ISS-Live-Location/./static/media/ISS.ef92b64d.jpg"
+  },
+  {
+    "revision": "f2cb24e0f7bca93de57a",
+    "url": "/ISS-Live-Location/./static/js/runtime~main.c1ba383e.js"
+  },
+  {
+    "revision": "8ccff78ec20b6cb17881",
+    "url": "/ISS-Live-Location/./static/js/main.cbda45ef.chunk.js"
+  },
+  {
+    "revision": "9ad83d94f2d3d18d3bff",
+    "url": "/ISS-Live-Location/./static/js/2.6d39aae6.chunk.js"
+  },
+  {
+    "revision": "8ccff78ec20b6cb17881",
+    "url": "/ISS-Live-Location/./static/css/main.a5d5d90b.chunk.css"
+  },
+  {
+    "revision": "9ad83d94f2d3d18d3bff",
+    "url": "/ISS-Live-Location/./static/css/2.579b8af9.chunk.css"
+  },
+  {
+    "revision": "ec0aa1f2961ad07600652fb89952dfff",
+    "url": "/ISS-Live-Location/./index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ISS-Live-Location/./precache-manifest.c1acbf5d37cb6951c3055f2dbea378c1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ISS-Live-Location/./index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
